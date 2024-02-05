60db4b1e1798e7e12ff924b00105677592bcd417
Dren Kastrati <dk56015@ubt-uni.net>
2024-02-05 10:49:03 +0100

    feat: Adding the GetVillas Method

--- Villa_API/Controllers/VillaController.cs
+++ Villa_API/Controllers/VillaController.cs
@@ -1,10 +1,21 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Villa_API.Models;
 
 namespace Villa_API.Controllers
 {
+    [Route("api/Villa")]
     [ApiController]
     public class VillaController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa{Id=1, Name="Pool View"},
+                new Villa{Id=2, Name="Beach View"}
+            };
+        }
     }
 }
