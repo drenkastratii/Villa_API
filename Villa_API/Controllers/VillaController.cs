fbd2fdd6c5026ed21affa7157818c1e532f34771
Dren Kastrati <dk56015@ubt-uni.net>
2024-02-05 15:30:12 +0100

    feat: Adding the CreateVilla method

--- Villa_API/Controllers/VillaController.cs
+++ Villa_API/Controllers/VillaController.cs
@@ -17,7 +17,7 @@ namespace Villa_API.Controllers
             return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("id")]
+        [HttpGet("{id:int}", Name = "GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -35,5 +35,27 @@ namespace Villa_API.Controllers
             }
             return villa;
         }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
+        {
+            if(villaDTO == null)
+            {
+                return BadRequest();
+            }
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u=>u.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villaDTO);
+
+            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id}, villaDTO);
+        }
+
+
     }
 }
