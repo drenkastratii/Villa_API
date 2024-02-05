b09e4155fcb5ba37f201509ecd530b75e8639f79
Dren Kastrati <dk56015@ubt-uni.net>
2024-02-05 19:24:55 +0100

    feat: Adding more properties to the VillaDTO

--- Villa_API/Models/DTO/VillaDTO.cs
+++ Villa_API/Models/DTO/VillaDTO.cs
@@ -8,7 +8,13 @@ namespace Villa_API.Models.DTO
         [Required]
         [MaxLength(30)]
         public string Name { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
         public int Occupany { get; set; }
         public int Sqft { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+        
     }
 }
