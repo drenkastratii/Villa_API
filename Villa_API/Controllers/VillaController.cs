227efc34fe5c856b86f57a2dc777869d7337aa23
Dren Kastrati <dk56015@ubt-uni.net>
2024-02-05 10:34:55 +0100

    feat: Adding our first controller

---
+++ Villa_API/Controllers/VillaController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Villa_API.Controllers
+{
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+    }
+}
