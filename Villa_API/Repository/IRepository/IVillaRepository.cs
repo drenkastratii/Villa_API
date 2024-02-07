612125b0d03e7ded0ef364c63190c1524fdc8e3c
Dren Kastrati <dk56015@ubt-uni.net>
2024-02-07 13:12:12 +0100

    fix: Adding the output results for the linq expressions

--- Villa_API/Repository/IRepository/IVillaRepository.cs
+++ Villa_API/Repository/IRepository/IVillaRepository.cs
@@ -5,8 +5,8 @@ namespace Villa_API.Repository.IRepository
 {
     public interface IVillaRepository
     {
-        Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null);
-        Task<Villa> Get(Expression<Func<Villa>> filter, bool tracked = true);
+        Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null);
+        Task<Villa> Get(Expression<Func<Villa, bool>> filter, bool tracked = true);
         Task Create(Villa entity);
         Task Update(Villa entity);
         Task Remove(Villa entity);
---
+++ Villa_API/Repository/VillaRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq.Expressions;
+using Villa_API.Data;
+using Villa_API.Models;
+using Villa_API.Repository.IRepository;
+
+namespace Villa_API.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public async Task Create(Villa entity)
+        {
+            await _db.Villas.AddAsync(entity);
+            await Save();
+        }
+
+        public Task<Villa> Get(Expression<Func<Villa>> filter, bool tracked = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null)
+        {
+            
+        }
+
+        public async Task Remove(Villa entity)
+        {
+            _db.Villas.Remove(entity);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+
+        public Task Update(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
